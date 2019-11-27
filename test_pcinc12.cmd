32e3cec6d9a698ae4fa803de314db3c6e0bbef31
Lenny Khazan <lenny.khazan@gmail.com>
2019-11-27 12:57:14 -0500

    fixed delays, added test files

---
+++ test_pcinc12.cmd
@@ -0,0 +1,15 @@
+ana GND! Vdd! out\[0\]/t out\[0\]/f out\[1\]/t out\[1\]/f out\[2\]/t out\[2\]/f out\[3\]/t out\[3\]/f out\[4\]/t out\[4\]/f out\[5\]/t out\[5\]/f out\[6\]/t out\[6\]/f out\[7\]/t out\[7\]/f out\[8\]/t out\[8\]/f out\[9\]/t out\[9\]/f out\[10\]/t out\[10\]/f out\[11\]/t out\[11\]/f go_r
+h Vdd! Reset
+l GND! go_r _Reset
+s
+
+l Reset
+h _Reset
+s
+
+l pc\[0\]/f pc\[1\]/f pc\[2\]/f pc\[3\]/f pc\[4\]/f pc\[5\]/f pc\[6\]/f pc\[7\]/f pc\[8\]/f pc\[9\]/f pc\[10\]/f pc\[11\]/f
+h pc\[0\]/t pc\[1\]/t pc\[2\]/t pc\[3\]/t pc\[4\]/t pc\[5\]/t pc\[6\]/t pc\[7\]/t pc\[8\]/t pc\[9\]/t pc\[10\]/t pc\[11\]/t
+
+h go_r
+s
+
