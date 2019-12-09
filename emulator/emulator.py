41df985f86610a2639ad13cf6d66be9ac23bfb14
Lenny Khazan <lenny.khazan@gmail.com>
2019-12-09 17:27:12 -0500

    generation of end-to-end prsim tests from emulator

--- emulator/emulator.py
+++ emulator/emulator.py
@@ -1,5 +1,8 @@
 import sys
 import math
+import copy
+
+GEN_PRSIM = True
 
 def compute(op, args, state):
     if op == 'halt':
@@ -9,7 +12,8 @@ def compute(op, args, state):
     elif op == 'pop':
         state['stack'].pop()
     elif op == 'popout':
-        print(state['stack'].pop())
+        if not GEN_PRSIM:
+            print(state['stack'].pop())
     elif op == 'pushreg':
         state['stack'].append(state['regs'][int(args[0])])
     elif op == 'popreg':
@@ -26,21 +30,194 @@ def compute(op, args, state):
         return int(args[0]) if state['stack'][-1] == 0 else 1
     elif op == 'brlz':
         return int(args[0]) if state['stack'][-1] < 0 else 1
+    else:
+        print('cannot parse op ' + op)
+        sys.exit(1)
 
     return 1
 
+def encode(parts):
+    op_encoded = 0
+    data_encoded = 0
+    if parts[0] == 'halt':
+        op_encoded = 0b0000
+    elif parts[0] == 'pushc':
+        op_encoded = 0b0001
+        data_encoded = int(parts[1])
+    elif parts[0] == 'pop':
+        op_encoded = 0b0010
+    elif parts[0] == 'popout':
+        op_encoded = 0b0011
+    elif parts[0] == 'pushreg':
+        op_encoded = 0b0100
+        data_encoded = int(parts[1])
+    elif parts[0] == 'popreg':
+        op_encoded = 0b0101
+        data_encoded = int(parts[1])
+    elif parts[0] == 'add':
+        op_encoded = 0b0110
+    elif parts[0] == 'sub':
+        op_encoded = 0b0111
+    elif parts[0] == 'jmp':
+        op_encoded = 0b1000
+        data_encoded = int(parts[1])
+    elif parts[0] == 'brz':
+        op_encoded = 0b1001
+        data_encoded = int(parts[1])
+    elif parts[0] == 'brlz':
+        op_encoded = 0b1010
+        data_encoded = int(parts[1])
+    else:
+        print('cannot encode op ' + parts[0])
+        sys.exit(1)
+
+    return (op_encoded << 8) | data_encoded
 
 def run(fname):
     state = { 'regs': [0] * 4, 'stack': [] }
+    prsim_preamble()
+
     with open(fname, 'r') as f:
         cmds = f.readlines()
         pc = 0
         while pc >= 0:
+            assert_state(state, pc)
+            assert_send('chan_PC', 12, pc)
             parts = [x.strip() for x in cmds[pc].split(' ')]
+            assert_recv('chan_IN', 12, encode(parts))
+            if parts[0] == 'popout':
+                assert_send('chan_OUT', 12, state['stack'][-1])
             pc = pc + compute(parts[0], parts[1:], state)
-            print(str(state), ' '.join(parts))
+            if not GEN_PRSIM:
+                print(str(state), ' '.join(parts))
 
-    print(state)
+    if not GEN_PRSIM:
+        print(state)
+
+def assert_send(channame, bitwidth, value):
+    if not GEN_PRSIM:
+        return
+
+    for i in range(bitwidth):
+        t = value & (0b1 << i)
+        if t:
+            print(f'assert {channame}[{i}].d.t 1')
+            print(f'assert {channame}[{i}].d.f 0')
+        else:
+            print(f'assert {channame}[{i}].d.t 0')
+            print(f'assert {channame}[{i}].d.f 1')
+
+    print(f'set {channame}.a 1')
+    print('cycle')
+
+    for i in range(bitwidth):
+        print(f'assert {channame}[{i}].d.t 0')
+        print(f'assert {channame}[{i}].d.f 0')
+
+    print(f'set {channame}.a 0')
+    print('cycle')
+
+def assert_recv(channame, bitwidth, value):
+    if not GEN_PRSIM:
+        return
+
+    for i in range(bitwidth):
+        t = value & (0b1 << i)
+        if t:
+            print(f'set {channame}[{i}].d.t 1')
+            print(f'set {channame}[{i}].d.f 0')
+        else:
+            print(f'set {channame}[{i}].d.t 0')
+            print(f'set {channame}[{i}].d.f 1')
+
+    print('cycle')
+    print(f'assert {channame}.a 1')
+    for i in range(bitwidth):
+        print(f'set {channame}[{i}].d.t 0')
+        print(f'set {channame}[{i}].d.f 0')
+
+    print('cycle')
+    print(f'assert {channame}.a 0')
+
+def assert_state(state, pc):
+    def assert_multibit(varname, bitwidth, target):
+        for i in range(bitwidth):
+            t = target & (0b1 << i)
+            if t:
+                print(f'assert {varname}[{i}].v.t 1')
+                print(f'assert {varname}[{i}].v.f 0')
+            else:
+                print(f'assert {varname}[{i}].v.t 0')
+                print(f'assert {varname}[{i}].v.f 1')
+
+    if not GEN_PRSIM:
+        return
+
+    for i in range(10):
+        if len(state['stack']) <= i:
+            print(f'assert var_stack{i}_is_full.v.t 0')
+            print(f'assert var_stack{i}_is_full.v.f 1')
+        else:
+            print(f'assert var_stack{i}_is_full.v.t 1')
+            print(f'assert var_stack{i}_is_full.v.f 0')
+            assert_multibit(f'var_stack{i}_data', 8, state['stack'][i])
+
+    for i in range(4):
+        assert_multibit(f'var_reg{i}', 8, state['regs'][i])
+
+    assert_multibit(f'var_pc_index', 12, pc)
+
+def prsim_preamble():
+    def watch_multibit(varname, bitwidth):
+        for i in range(bitwidth):
+            print(f'watch {varname}[{i}].v.t')
+            print(f'watch {varname}[{i}].v.f')
+
+    def watch_chan(channame, bitwidth):
+        for i in range(bitwidth):
+            print(f'watch {channame}.d[{i}].t')
+            print(f'watch {channame}.d[{i}].f')
+        print(f'watch {channame}.a')
+
+    def clear_chan_data(channame, bitwidth):
+        for i in range(bitwidth):
+                print(f'set {channame}.d[{i}].t 0')
+                print(f'set {channame}.d[{i}].f 0')
+
+    if not GEN_PRSIM:
+        return
+
+    print('initialize')
+    print('break-on-warn')
+    print('watch Reset')
+    print('watch _Reset')
+    print('watch go_r')
+    for i in range(10):
+        watch_multibit(f'var_stack{i}_data', 8)
+        print(f'watch var_stack{i}_is_full.v.t')
+        print(f'watch var_stack{i}_is_full.v.f')
+
+    for i in range(4):
+        watch_multibit(f'var_reg{i}', 8)
+
+    watch_multibit('var_pc_index', 12)
+
+    watch_chan('chan_PC', 12)
+    watch_chan('chan_IN', 12)
+    watch_chan('chan_OUT', 12)
+
+    print('mode reset')
+    print('set Reset 1')
+    print('set _Reset 0')
+    print('set chan_PC.a 0')
+    print('set chan_OUT.a 0')
+    clear_chan_data('chan_IN', 12)
+    print('cycle')
+    print('set Reset 0')
+    print('set _Reset 1')
+    print('cycle')
+
+    print('mode run')
 
 def main():
     if len(sys.argv) != 2:
